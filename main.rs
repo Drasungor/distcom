f20ed6bfb25babf412ec35d42f15acb2ce22038c
Agustín <aguscambiano@gmail.com>
2023-11-13 20:41:39 -0300

    Continue implementation of multipart file upload3

--- main.rs
+++ main.rs
@@ -35,26 +35,27 @@ async fn main() -> std::io::Result<()> {
 
 async fn upload_file(mut payload: Multipart) -> Result<String, actix_web::error::Error> {
     while let Ok(Some(field_result)) = payload.try_next().await {
-        let field = field_result;
+        // let field = field_result;
+        let mut field = field_result;
 
-        let content_type = field.content_type();
-        // let filename = match field.content_disposition() {
-        //     Some(cd) => cd.get_filename().unwrap_or("unknown"),
-        //     None => "unknown",
-        // };
+        // let content_type = field.content_type();
 
-        let filename = match field.content_disposition() {
-            Some(cd) => cd.get_filename().unwrap_or_else(|| "unknown".to_string()),
+        let filename = match field.content_disposition().get_filename() {
+            Some(cd) => cd.to_string(),
             None => "unknown".to_string(),
         };
-
+        
         // Define the file path where you want to save the uploaded file
         let file_path = format!("./uploads/{}", filename);
-
+        let file_path_clone = file_path.clone();
         // Create a new file and write the field data to it
-        let mut f = web::block(|| File::create(&file_path)).await?;
+        let mut f = web::block(|| File::create(file_path_clone)).await??;
         while let Some(chunk) = field.try_next().await? {
-            f = web::block(move || f.write_all(&chunk).map(|_| f)).await?;
+            // let aaaa = web::block(|| f.expect("REASON").write_all(&chunk).map(|_| f)).await?;
+            let aaaa = web::block(|| f.write_all(&chunk).map(|_| f)).await?;
+            let bbbb = aaaa.expect("ekisd");
+            // f = web::block(move || f.expect("REASON").write_all(&chunk).map(|_| f)).await?;
+            f = bbbb;
         }
 
         // You can return the file path or any other response as needed
