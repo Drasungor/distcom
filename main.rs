45434a5eaf1987320e9d50c6e21b0c732904e660
Agustín <aguscambiano@gmail.com>
2023-11-14 19:00:27 -0300

    Add uploads folder creation on file upload

--- main.rs
+++ main.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, App, HttpServer};
 use actix_multipart::Multipart;
+use std::fs; // Add import for File
 use std::fs::File; // Add import for File
 use std::io::Write; // Add import for Write
 use futures_util::stream::TryStreamExt;
@@ -26,11 +27,24 @@ async fn main() -> std::io::Result<()> {
                 web::scope("/goodbye")
                     .route("", web::get().to(handlers::goodbye::goodbye))
                     .route("/two", web::get().to(handlers::goodbye::goodbye_two))
+            ).service(
+                web::scope("/upload")
+                    .route("", web::post().to(upload_file))
             )
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
+}
+
+fn folder_exists(path: &str) -> bool {
+    fs::metadata(path).is_ok()
+}
+
+fn create_folder(path: &str) -> () {
+    if !folder_exists(path) {
+        fs::create_dir(path).expect("Error in uploads folder creation")
+    }
 }
 
 async fn upload_file(mut payload: Multipart) -> Result<String, actix_web::error::Error> {
@@ -45,15 +59,15 @@ async fn upload_file(mut payload: Multipart) -> Result<String, actix_web::error:
             None => "unknown".to_string(),
         };
         
+        let uploads_folder = "./uploads";
+        create_folder(uploads_folder);
         // Define the file path where you want to save the uploaded file
-        let file_path = format!("./uploads/{}", filename);
+        let file_path = format!("{}/{}", uploads_folder, filename);
         let file_path_clone = file_path.clone();
         // Create a new file and write the field data to it
         let f = web::block(|| File::create(file_path_clone)).await??;
         while let Some(chunk) = field.try_next().await? {
             let mut file_pointer_clone = f.try_clone()?;
-            // let chunk_clone = chunk.clone();
-            // web::block(|| f.write_all(&chunk)).await??;
             web::block(move || file_pointer_clone.write_all(&chunk)).await??;
         }
 
