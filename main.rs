a6c59de5efb38459490bbd0d24f80e11688229d4
Agustín <aguscambiano@gmail.com>
2023-11-28 19:49:51 -0300

    Delete comment

--- main.rs
+++ main.rs
@@ -37,32 +37,3 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
-
-// pub async fn upload_file(mut payload: Multipart) -> Result<String, actix_web::error::Error> {
-//     let mut file_paths: Vec<String> = Vec::new();
-//     let uploads_folder = "./uploads";
-//     create_folder(uploads_folder);
-//     while let Ok(Some(field_result)) = payload.try_next().await {
-//         let mut field = field_result;
-//         let filename = match field.content_disposition().get_filename() {
-//             Some(cd) => cd.to_string(),
-//             None => "unknown".to_string(),
-//         };
-        
-//         // Define the file path where you want to save the uploaded file
-//         let file_path = format!("{}/{}", uploads_folder, filename);
-//         let file_path_clone = file_path.clone();
-//         // Create a new file and write the field data to it
-//         let f = web::block(|| File::create(file_path_clone)).await??;
-//         while let Some(chunk) = field.try_next().await? {
-//             let mut file_pointer_clone = f.try_clone()?;
-//             web::block(move || file_pointer_clone.write_all(&chunk)).await??;
-//         }
-//         file_paths.push(file_path);
-//     }
-//     if file_paths.is_empty() {
-//         Ok("No file uploaded".to_string())
-//     } else {
-//         Ok(format!("Files saved at: {:?}", file_paths))
-//     }
-// }
