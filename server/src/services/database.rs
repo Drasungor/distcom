f0e13d627bad7ac8af9db2d3e08cef703ce0bfcb
Agustín <aguscambiano@gmail.com>
2024-01-23 00:28:12 -0300

    Start addition of postgress database

---
+++ server/src/services/database.rs
@@ -0,0 +1,24 @@
+use serde_derive::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::Read;
+use lazy_static::lazy_static;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Config {
+   pub x: i32,
+   pub y: i32,
+}
+
+lazy_static! {
+    pub static ref CONFIG_OBJECT: Config = load_config("./src/config/dev.json").unwrap();
+}
+
+
+fn load_config(path: &str) -> Result<Config, Box<dyn std::error::Error>> {
+    let mut file = File::open(path)?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+    let config_object: Config = serde_json::from_str(&content)?;
+
+    Ok(config_object)
+}
