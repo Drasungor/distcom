4d5949e5d7cd83b3bed75061380458b01b7a2a44
Agustín <aguscambiano@gmail.com>
2024-01-24 00:37:43 -0300

    Update diesel import and database connection code

--- server/src/services/database.rs
+++ server/src/services/database.rs
@@ -1,19 +1,21 @@
-use serde_derive::{Serialize, Deserialize};
-use std::fs::File;
-use std::io::Read;
+// use serde_derive::{Serialize, Deserialize};
+// use std::fs::File;
+// use std::io::Read;
 use lazy_static::lazy_static;
 // use crate::common;
 use crate::common::config;
+// use diesel::pg::PgConnection;
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+// use diesel::prelude::*;
 
 lazy_static! {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    // let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     // let connection = PgConnection::establish(&database_url).unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
-    pub static ref connection = PgConnection::establish(config::CONFIG_OBJECT).unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
-    println!("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-
+    pub static ref CONNECTION: PgConnection = PgConnection::establish(config::CONFIG_OBJECT.database_url).unwrap_or_else(|_| panic!("Error connecting to {}", config::CONFIG_OBJECT.database_url));
+    // println!("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 }
 
-pub fn
 
 // fn load_config(path: &str) -> Result<Config, Box<dyn std::error::Error>> {
 //     let mut file = File::open(path)?;
