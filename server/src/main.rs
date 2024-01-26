18f68e87ebd4fe2f35fda2263f5c43c105372ef4
Agustín <aguscambiano@gmail.com>
2024-01-25 22:43:05 -0300

    Start adding app state to actix web and pooling for diesel using r2d2 module

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,12 +3,27 @@ mod handlers;
 mod middlewares;
 mod common;
 mod services;
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use diesel::r2d2::Pool;
+use diesel::r2d2::ConnectionManager;
+// use r2d2_diesel::ConnectionManager;
+
+
+// #[derive(Clone)]
+struct AppState {
+    db: PgConnection
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("{:?}", common::config::CONFIG_OBJECT.x);
     // println!("{:?}", common::lazy_initialize::CONFIG_OBJECT.x);
     // println!("{:?}", common::config::CONFIG_OBJECT.database_url);
+    let database_url = common::config::CONFIG_OBJECT.database_url.as_str();
+    let database_connection: PgConnection = PgConnection::establish(database_url).unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+
+
     HttpServer::new(|| {
         App::new()
             .service(
--- server/src/services/database.rs
+++ server/src/services/database.rs
@@ -1,27 +1,27 @@
-// use serde_derive::{Serialize, Deserialize};
-// use std::fs::File;
-// use std::io::Read;
-use lazy_static::lazy_static;
-// use crate::common;
-use crate::common::config;
+// // use serde_derive::{Serialize, Deserialize};
+// // use std::fs::File;
+// // use std::io::Read;
+// use lazy_static::lazy_static;
+// // use crate::common;
+// // use diesel::pg::PgConnection;
+// use crate::common::config;
 // use diesel::pg::PgConnection;
-use diesel::pg::PgConnection;
-use diesel::prelude::*;
 // use diesel::prelude::*;
+// // use diesel::prelude::*;
 
-lazy_static! {
-    // let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    // let connection = PgConnection::establish(&database_url).unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
-    pub static ref CONNECTION: PgConnection = PgConnection::establish(config::CONFIG_OBJECT.database_url).unwrap_or_else(|_| panic!("Error connecting to {}", config::CONFIG_OBJECT.database_url));
-    // println!("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-}
-
-
-// fn load_config(path: &str) -> Result<Config, Box<dyn std::error::Error>> {
-//     let mut file = File::open(path)?;
-//     let mut content = String::new();
-//     file.read_to_string(&mut content)?;
-//     let config_object: Config = serde_json::from_str(&content)?;
-
-//     Ok(config_object)
+// lazy_static! {
+//     // let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+//     // let connection = PgConnection::establish(&database_url).unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+//     pub static ref CONNECTION: PgConnection = PgConnection::establish(config::CONFIG_OBJECT.database_url).unwrap_or_else(|_| panic!("Error connecting to {}", config::CONFIG_OBJECT.database_url));
+//     // println!("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 // }
+
+
+// // fn load_config(path: &str) -> Result<Config, Box<dyn std::error::Error>> {
+// //     let mut file = File::open(path)?;
+// //     let mut content = String::new();
+// //     file.read_to_string(&mut content)?;
+// //     let config_object: Config = serde_json::from_str(&content)?;
+
+// //     Ok(config_object)
+// // }
