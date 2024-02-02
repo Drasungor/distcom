dc3c5e84d00605118c0cc28cea941d3f3a17bfe1
Agustín <aguscambiano@gmail.com>
2024-02-02 00:22:08 -0300

    Continue trying to fix unhealthy db problem

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,7 +3,8 @@ mod handlers;
 mod middlewares;
 mod common;
 mod services;
-use diesel::pg::PgConnection;
+// use diesel::pg::PgConnection;
+use diesel::mysql::MysqlConnection;
 use diesel::prelude::*;
 use diesel::r2d2::Pool;
 use diesel::r2d2::ConnectionManager;
@@ -12,7 +13,8 @@ use diesel::r2d2::ConnectionManager;
 
 // #[derive(Clone)]
 struct AppState {
-    db: PgConnection
+    // db_connection_pool: Pool<ConnectionManager<PgConnection>>
+    db_connection_pool: Pool<ConnectionManager<MysqlConnection>>
 }
 
 #[actix_web::main]
@@ -21,11 +23,19 @@ async fn main() -> std::io::Result<()> {
     // println!("{:?}", common::lazy_initialize::CONFIG_OBJECT.x);
     // println!("{:?}", common::config::CONFIG_OBJECT.database_url);
     let database_url = common::config::CONFIG_OBJECT.database_url.as_str();
-    let database_connection: PgConnection = PgConnection::establish(database_url).unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+    // let database_connection: PgConnection = PgConnection::establish(database_url).unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+    // let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let manager = ConnectionManager::<MysqlConnection>::new(database_url);
 
+    let connection_pool = Pool::builder().test_on_check_out(true).build(manager).expect("Could not build connection pool");
 
-    HttpServer::new(|| {
+    let state = web::Data::new(AppState { db_connection_pool: connection_pool });
+
+    println!("ekisdddddddddddddddddddddddddddddddddddd");
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(state.clone())
             .service(
                 web::scope("/")
                     .route("", web::get().to(handlers::index::index))
