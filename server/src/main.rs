9cc79bc8ff237a453801b92c61c9a5e5dfae99fb
Agustin <aguscambieno@gmail.com>
2024-02-02 21:34:33 -0300

    Delete comments

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,28 +3,19 @@ mod handlers;
 mod middlewares;
 mod common;
 mod services;
-// use diesel::pg::PgConnection;
 use diesel::mysql::MysqlConnection;
 use diesel::prelude::*;
 use diesel::r2d2::Pool;
 use diesel::r2d2::ConnectionManager;
-// use r2d2_diesel::ConnectionManager;
 
-
-// #[derive(Clone)]
 struct AppState {
-    // db_connection_pool: Pool<ConnectionManager<PgConnection>>
     db_connection_pool: Pool<ConnectionManager<MysqlConnection>>
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("{:?}", common::config::CONFIG_OBJECT.x);
-    // println!("{:?}", common::lazy_initialize::CONFIG_OBJECT.x);
-    // println!("{:?}", common::config::CONFIG_OBJECT.database_url);
     let database_url = common::config::CONFIG_OBJECT.database_url.as_str();
-    // let database_connection: PgConnection = PgConnection::establish(database_url).unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
-    // let manager = ConnectionManager::<PgConnection>::new(database_url);
     let manager = ConnectionManager::<MysqlConnection>::new(database_url);
 
     let connection_pool = Pool::builder().test_on_check_out(true).build(manager).expect("Could not build connection pool");
