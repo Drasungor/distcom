49a73861f31c5ddc3e30d8e335a4050fb8065c55
Agustin <aguscambieno@gmail.com>
2024-02-08 20:53:02 -0300

    Update database schema

--- server/src/schema.rs
+++ server/src/schema.rs
@@ -3,10 +3,32 @@
 diesel::table! {
     account (organization_id) {
         organization_id -> Bigint,
+        
         #[max_length = 255]
         name -> Varchar,
+        
         #[max_length = 255]
         description -> Varchar,
+        
         account_was_verified -> Bool,
+        
+        #[max_length = 255]
+        username -> Varchar,
+
+        #[max_length = 255]
+        password -> Varchar,
     }
 }
+
+diesel::table! {
+    refresh_token (token_id) {
+        token_id -> Bigint,
+
+        user_id -> Bigint,
+    }
+}
+
+// diesel::allow_tables_to_appear_in_same_query!(
+//     account,
+//     papafrita,
+// );
