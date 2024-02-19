43f582f1b0bbab9dc830c82051c2483054284dd0
Agustin <aguscambieno@gmail.com>
2024-02-18 23:55:51 -0300

    .

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,24 +1,14 @@
 use actix_web::{web, App, HttpServer};
-// use diesel::migration::MigrationSource;
 use diesel::mysql::MysqlConnection;
-// use diesel::mysql::Mysql;
 use diesel::prelude::*;
-use diesel::r2d2::Pool;
-use diesel::r2d2::ConnectionManager;
-use diesel::r2d2::R2D2Connection;
-// use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
-
 use diesel_async::{RunQueryDsl, AsyncConnection, AsyncMysqlConnection};
-// use diesel_async::{RunQueryDsl, AsyncConnection, AsyncPgConnection};
-// use diesel_async_migrations::{EmbeddedMigrations, embed_migrations};
+use diesel_async::pooled_connection::AsyncDieselConnectionManager;
+use diesel_async::pooled_connection::deadpool::Pool;
 use mysql_diesel_async_migration::EmbeddedMigrations;
 use embed_migrations_macro_function::mysql_embed_migrations;
 
 // Copied implementation from
 // https://github.com/diesel-rs/diesel/blob/master/guide_drafts/migration_guide.md
-// pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
-
-// pub const MIGRATIONS: diesel_async_migrations::EmbeddedMigrations = diesel_async_migrations::embed_migrations!();
 pub const MIGRATIONS: EmbeddedMigrations = mysql_embed_migrations!();
 
 
@@ -32,23 +22,30 @@ mod utils;
 
 #[derive(Clone)]
 struct AppState {
-    // db_connection_pool: Pool<ConnectionManager<MysqlConnection>>
+    // db_connection_pool: Pool<ConnectionManager<MysqlConnection>>,
+    db_connection_pool: Pool<AsyncDieselConnectionManager<AsyncMysqlConnection>>,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("{:?}", common::config::CONFIG_OBJECT.x);
     let database_url = common::config::CONFIG_OBJECT.database_url.as_str();
-    let mut connection = AsyncMysqlConnection::establish(database_url).await.expect("text2");
-    MIGRATIONS.run_pending_migrations(&mut connection).await.expect("The migration failed");
+
+
+    let connection_manager = AsyncDieselConnectionManager::<diesel_async::AsyncMysqlConnection>::new(database_url);
+    let pool = Pool::builder(connection_manager).build()?;
+    let mut conn = pool.get().await?;
+    MIGRATIONS.run_pending_migrations(&mut conn).await.expect("The migration failed");
+
+
+    // let mut connection = AsyncMysqlConnection::establish(database_url).await.expect("text2");
+    // MIGRATIONS.run_pending_migrations(&mut connection).await.expect("The migration failed");
+
+
     // let state = web::Data::new(AppState { db_connection_pool: connection_pool });
     let state = web::Data::new(AppState { });
 
     println!("ekisdddddddddddddddddddddddddddddddddddd");
-
-    // let query_result = connection_pool_copy.get().expect("exploto el get de conexion").ping();
-    // query_result.expect("Error in database ping");
-
     println!("pase el ping");
 
     HttpServer::new(move || {
