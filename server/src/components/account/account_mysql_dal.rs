31d9e07908b8d8455ea31044cd810a7b905b1720
Agustin <aguscambieno@gmail.com>
2024-02-19 01:10:42 -0300

    Return to original sync pooled diesel with embeded migrations implementation

--- server/src/components/account/account_mysql_dal.rs
+++ server/src/components/account/account_mysql_dal.rs
@@ -1,29 +1,32 @@
-use diesel_async::AsyncMysqlConnection;
-use diesel_async::RunQueryDsl;
+// use diesel_async::AsyncMysqlConnection;
+// use diesel_async::RunQueryDsl;
 
 // use super::{dal::AccountDal, db_models::account::NewAccount};
 use super::{dal::AccountDal, db_models::account::{CompleteAccount}};
 
 // Define a struct that will implement the Printable trait
-struct AccountMysqlDal<'a> {
-    database_connection: &'a mut AsyncMysqlConnection,
+struct AccountMysqlDal {
+// struct AccountMysqlDal<'a> {
+    // database_connection: &'a mut AsyncMysqlConnection,
 }
 
-impl<'a> AccountMysqlDal<'a> {
-    fn new(database_connection: &'a mut AsyncMysqlConnection) -> AccountMysqlDal<'a> {
-        return AccountMysqlDal{ database_connection };
-    }
+impl AccountMysqlDal {
+// impl<'a> AccountMysqlDal<'a> {
+    // fn new(database_connection: &'a mut AsyncMysqlConnection) -> AccountMysqlDal<'a> {
+    //     return AccountMysqlDal{ database_connection };
+    // }
 }
 
 
-impl<'a> AccountDal for AccountMysqlDal<'a> {
+impl AccountDal for AccountMysqlDal {
+// impl<'a> AccountDal for AccountMysqlDal<'a> {
     // async fn register_account(&self, new_account_data: &NewAccount) {
     async fn register_account(&mut self, new_account_data: &CompleteAccount) {
-        diesel::insert_into(crate::schema::account::table)
-            .values(new_account_data)
-            .execute(self.database_connection)
-            .await
-            .expect("Error saving new post");
+        // diesel::insert_into(crate::schema::account::table)
+        //     .values(new_account_data)
+        //     .execute(self.database_connection)
+        //     .await
+        //     .expect("Error saving new post");
     }
 
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,20 @@
 use actix_web::{web, App, HttpServer};
 use diesel::mysql::MysqlConnection;
 use diesel::prelude::*;
-use diesel_async::{RunQueryDsl, AsyncConnection, AsyncMysqlConnection};
-use diesel_async::pooled_connection::AsyncDieselConnectionManager;
-use diesel_async::pooled_connection::deadpool::Pool;
-use mysql_diesel_async_migration::EmbeddedMigrations;
-use embed_migrations_macro_function::mysql_embed_migrations;
+// use diesel_async::{RunQueryDsl, AsyncConnection, AsyncMysqlConnection};
+// use diesel_async::pooled_connection::AsyncDieselConnectionManager;
+// use diesel_async::pooled_connection::deadpool::Pool;
+use diesel::r2d2::ConnectionManager;
+use diesel::r2d2::Pool;
+use diesel::r2d2::R2D2Connection;
+// use mysql_diesel_async_migration::EmbeddedMigrations;
+// use embed_migrations_macro_function::mysql_embed_migrations;
+use diesel_migrations::{ embed_migrations, EmbeddedMigrations, MigrationHarness };
 
 // Copied implementation from
 // https://github.com/diesel-rs/diesel/blob/master/guide_drafts/migration_guide.md
-pub const MIGRATIONS: EmbeddedMigrations = mysql_embed_migrations!();
+// pub const MIGRATIONS: EmbeddedMigrations = mysql_embed_migrations!();
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 
 mod handlers;
@@ -22,8 +27,8 @@ mod utils;
 
 #[derive(Clone)]
 struct AppState {
-    // db_connection_pool: Pool<ConnectionManager<MysqlConnection>>,
-    db_connection_pool: Pool<AsyncDieselConnectionManager<AsyncMysqlConnection>>,
+    db_connection_pool: Pool<ConnectionManager<MysqlConnection>>,
+    // db_connection_pool: Pool<AsyncDieselConnectionManager<AsyncMysqlConnection>>,
 }
 
 #[actix_web::main]
@@ -32,18 +37,26 @@ async fn main() -> std::io::Result<()> {
     let database_url = common::config::CONFIG_OBJECT.database_url.as_str();
 
 
-    let connection_manager = AsyncDieselConnectionManager::<diesel_async::AsyncMysqlConnection>::new(database_url);
-    let pool = Pool::builder(connection_manager).build()?;
-    let mut conn = pool.get().await?;
-    MIGRATIONS.run_pending_migrations(&mut conn).await.expect("The migration failed");
+    // let connection_manager = AsyncDieselConnectionManager::<diesel_async::AsyncMysqlConnection>::new(database_url);
+    // let pool = Pool::builder(connection_manager).build()?;
+
+    let manager = ConnectionManager::<MysqlConnection>::new(database_url);
+    let connection_pool = Pool::builder().test_on_check_out(true).build(manager).expect("Failed to create pool");
+
+    let mut pooled_connection = connection_pool.get().expect("asdasdas");
+
+    // let mut conn = pool.get().await?;
+    // MIGRATIONS.run_pending_migrations(&mut conn).await.expect("The migration failed");
 
 
     // let mut connection = AsyncMysqlConnection::establish(database_url).await.expect("text2");
     // MIGRATIONS.run_pending_migrations(&mut connection).await.expect("The migration failed");
+    pooled_connection.run_pending_migrations(MIGRATIONS).expect("The migration failed");
+
 
 
     // let state = web::Data::new(AppState { db_connection_pool: connection_pool });
-    let state = web::Data::new(AppState { });
+    let state = web::Data::new(AppState { db_connection_pool: connection_pool });
 
     println!("ekisdddddddddddddddddddddddddddddddddddd");
     println!("pase el ping");
