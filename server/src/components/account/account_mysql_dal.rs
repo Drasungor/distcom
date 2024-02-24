fcc6bc6a1bccea368b49e55f12391e5420f3b26b
Agustin <aguscambieno@gmail.com>
2024-02-24 18:07:10 -0300

    Add transaction to account registering

--- server/src/components/account/account_mysql_dal.rs
+++ server/src/components/account/account_mysql_dal.rs
@@ -1,6 +1,7 @@
 // use diesel_async::AsyncMysqlConnection;
 // use diesel_async::RunQueryDsl;
 
+use diesel::connection;
 // use super::{dal::AccountDal, db_models::account::NewAccount};
 use diesel::RunQueryDsl;
 use diesel::prelude::*;
@@ -18,10 +19,18 @@ impl AccountMysqlDal {
 
     pub async fn register_account(new_account_data: CompleteAccount) {
         let mut connection = crate::common::config::CONNECTION_POOL.get().expect("get connection failure");
-        web::block(move || diesel::insert_into(account::table)
-        .values(&new_account_data)
-        .execute(&mut connection)
-        .expect("Error saving new post")).await.expect("Error in future await");
+        web::block(move || {
+        connection.transaction::<_, diesel::result::Error, _>(|connection| {
+
+            diesel::insert_into(account::table)
+                    .values(&new_account_data)
+                //    .execute(&mut connection)
+                    .execute(connection)
+                    .expect("Error saving new post");
+            return Ok(());
+
+        }).expect("asdasdasd");
+        }).await.expect("Error in future await")
     }
 
     pub async fn get_account_data_by_username(username: String) -> CompleteAccount {
