b5681f091323a8cd403aab6c199968a89bd2bf72
Agustin <aguscambieno@gmail.com>
2024-03-16 20:49:31 -0300

    .

---
+++ risc_0_examples/basic_prime_test/host/src/main.rs
@@ -0,0 +1,41 @@
+use methods::{
+    BASIC_PRIME_TEST_GUEST_ELF, BASIC_PRIME_TEST_GUEST_ID
+};
+use risc0_zkvm::{default_prover, ExecutorEnv};
+use std::env;
+use basic_prime_test_core;
+
+fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
+        .init();
+
+    let args: Vec<String> = env::args().collect();
+
+    println!("The args list is: {:?}", args);
+
+    if (args.len() != 2) {
+        panic!("The program needs a number for primality check");
+    }
+
+    let tested_number = args[1].parse::<u32>().expect("The received input is not a positive integer");
+
+    let env = ExecutorEnv::builder()
+        .write(&tested_number)
+        .unwrap()
+        .build()
+        .unwrap();
+
+    let prover = default_prover();
+    let receipt = prover
+        .prove(env, BASIC_PRIME_TEST_GUEST_ELF)
+        .unwrap();
+
+    let _output: basic_prime_test_core::Outputs = receipt.journal.decode().unwrap();
+
+    println!("The output of the journal is {:?}", _output);
+
+    receipt
+        .verify(BASIC_PRIME_TEST_GUEST_ID)
+        .unwrap();
+}
