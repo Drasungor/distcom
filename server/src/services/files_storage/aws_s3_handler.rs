526ccdf39579df5fed611a814bb30d29251251fa
Agustin <aguscambieno@gmail.com>
2024-03-26 00:28:39 -0300

    Add mime_guess

--- server/src/services/files_storage/aws_s3_handler.rs
+++ server/src/services/files_storage/aws_s3_handler.rs
@@ -9,12 +9,24 @@ pub struct AwsS3Handler;
 #[async_trait]
 impl FileStorage for AwsS3Handler {
 
+
+
     async fn upload(&self, file_path: &str) -> Result<(), AppError> {
         Ok(())
     }
 
     async fn delete(&self) -> Result<(), AppError> {
         Ok(())
+    }
+
+
+
+}
+
+impl AwsS3Handler {
+
+    async fn new(uploaded_files_url: &str) -> Result<AwsS3Handler, AppError> {
+        return Ok(AwsS3Handler {});
     }
 
 }
