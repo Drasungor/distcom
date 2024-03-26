d7c4dcebe72fba4a85ec3d6f04484b2ffc71beec
Agustin <aguscambieno@gmail.com>
2024-03-26 01:29:11 -0300

    Add aws config loading and s3 client initialization

--- server/src/services/files_storage/aws_s3_handler.rs
+++ server/src/services/files_storage/aws_s3_handler.rs
@@ -1,10 +1,13 @@
 use async_trait::async_trait;
+use aws_sdk_s3 as s3;
+use aws_config;
 
 use crate::common::app_error::AppError;
-
 use super::file_storage::FileStorage;
 
-pub struct AwsS3Handler;
+pub struct AwsS3Handler {
+    s3_client: s3::Client,
+}
 
 #[async_trait]
 impl FileStorage for AwsS3Handler {
@@ -25,10 +28,12 @@ impl FileStorage for AwsS3Handler {
 
 impl AwsS3Handler {
 
+    // TODO: Check if this function can be async, or if the initialization of s3_client should be done in another method
     async fn new(uploaded_files_url: &str) -> Result<AwsS3Handler, AppError> {
-        return Ok(AwsS3Handler {});
+        let my_config = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
+        return Ok(AwsS3Handler {
+            s3_client: s3::Client::new(&my_config),
+        });
     }
-
-    
 
 }
