82df50f1775c1ee0def6f1a89dc6ecbbc1fcb099
Agustin <aguscambieno@gmail.com>
2024-03-26 22:17:40 -0300

    Implement s3 upload function

--- server/src/services/files_storage/aws_s3_handler.rs
+++ server/src/services/files_storage/aws_s3_handler.rs
@@ -1,12 +1,17 @@
+use std::path::Path;
+
 use async_trait::async_trait;
 use aws_sdk_s3 as s3;
 use aws_config;
+use s3::primitives::ByteStream;
+// use std::path::Path;
 
-use crate::common::app_error::AppError;
+use crate::common::app_error::{AppError, AppErrorType};
 use super::file_storage::FileStorage;
 
 pub struct AwsS3Handler {
     s3_client: s3::Client,
+    bucket_name: String,
 }
 
 #[async_trait]
@@ -14,8 +19,40 @@ impl FileStorage for AwsS3Handler {
 
 
 
-    async fn upload(&self, file_path: &str) -> Result<(), AppError> {
-        Ok(())
+    async fn upload(&self, file_path: &Path) -> Result<(), AppError> {
+    // Path::new();
+        if (!file_path.exists()) {
+            return Err(AppError::new(AppErrorType::InternalServerError));
+        }
+        
+        let key: &str;
+        match file_path.to_str() {
+            Some(stringified_path) => {
+                key = stringified_path;
+            },
+            None => {
+                return Err(AppError::new(AppErrorType::InternalServerError));
+            }
+        }
+
+        let body: ByteStream;
+        match ByteStream::from_path(file_path).await {
+            Ok(generated_bytestream) => {
+                body = generated_bytestream;
+            },
+            Err(_) => {
+                return Err(AppError::new(AppErrorType::InternalServerError));
+            }
+        }
+
+        let content_type = mime_guess::from_path(file_path).first_or_octet_stream().to_string();
+        let req = self.s3_client.put_object().bucket(self.bucket_name.clone()).key(key).
+                                            body(body).content_type(content_type);
+
+        match req.send().await {
+            Ok(_) => Ok(()),
+            Err(_) => Err(AppError::new(AppErrorType::InternalServerError)),
+        }
     }
 
     async fn delete(&self) -> Result<(), AppError> {
@@ -33,6 +70,7 @@ impl AwsS3Handler {
         let my_config = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
         return Ok(AwsS3Handler {
             s3_client: s3::Client::new(&my_config),
+            bucket_name: "a".to_string(),
         });
     }
 
--- server/src/services/files_storage/file_storage.rs
+++ server/src/services/files_storage/file_storage.rs
@@ -1,9 +1,12 @@
+use std::path::Path;
+
 use async_trait::async_trait;
 
 use crate::common::app_error::AppError;
 
 #[async_trait]
 pub trait FileStorage {
-    async fn upload(&self, file_path: &str) -> Result<(), AppError>;
+    // async fn upload(&self, file_path: &str) -> Result<(), AppError>;
+    async fn upload(&self, file_path: &Path) -> Result<(), AppError>;
     async fn delete(&self) -> Result<(), AppError>;
 }
