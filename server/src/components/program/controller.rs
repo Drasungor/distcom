44d2fb63184c3c6d5b27314f1012b909512cdaba
Agustin <aguscambieno@gmail.com>
2024-04-01 14:12:11 -0300

    Merge uploaded files inside an organization folder

--- server/src/components/program/controller.rs
+++ server/src/components/program/controller.rs
@@ -16,19 +16,20 @@ impl ProgramController {
     pub async fn upload_program(req: HttpRequest, mut form: Multipart) -> impl Responder {
         let files_names = upload_file(form).await.expect("Failed file upload");
 
-        println!("Extensions asdasdasdasdasd: {:?}", req.extensions().get::<RequestExtension>());
+        // TODO: Change expect calls to an internal server error handling
+        let extension_value = req.extensions().get::<RequestExtension>().expect("Extension should be initialized").clone();
+        let jwt_payload = extension_value.jwt_payload.clone().expect("The jwt payload does not exist");
         
+        // println!("files_names: {:?}", files_names);
+
         for file_name in files_names {
-            // let file_path = format!("./upload/{}", file_name);
-            // fs::remove_file(file_path).expect("Error in file deletion");
             let file_path = format!("./uploads/{}", file_name);
-
-
+            let new_file_name = format!("{}/{}", jwt_payload.organization_id, file_name);
             {
                 let read_guard = common::config::FILES_STORAGE.read().expect("Error in rw lock");
-                read_guard.upload(Path::new(&file_path), &file_name).await.expect("File upload error");
+                println!("I am going to upload the files");
+                read_guard.upload(Path::new(&file_path), &new_file_name).await.expect("File upload error");
             }
-
             fs::remove_file(file_path).expect("Error in file deletion");
         }
 
