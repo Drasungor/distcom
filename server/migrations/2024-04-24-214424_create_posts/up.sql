3ee167e723ea0cfcf54db3ef09e47d31a943f742
Agustin <aguscambieno@gmail.com>
2024-04-24 19:39:08 -0300

    Add new migration files

---
+++ server/migrations/2024-04-24-214424_create_posts/up.sql
@@ -0,0 +1,35 @@
+-- Your SQL goes here
+CREATE TABLE `specific_program_input`(
+	`specific_input_id` VARCHAR(255) NOT NULL PRIMARY KEY,
+	`input_group_id` VARCHAR(255) NOT NULL,
+	`blob_data` VARBINARY(1024),
+	`order` INTEGER NOT NULL,
+	INDEX `input_group_id_index` (`input_group_id`)
+);
+
+CREATE TABLE `program_input_group`(
+	`input_group_id` VARCHAR(255) NOT NULL PRIMARY KEY,
+	`program_id` VARCHAR(255) NOT NULL,
+	`last_reserved` DATETIME
+);
+
+CREATE TABLE `program`(
+	`organization_id` VARCHAR(255) NOT NULL,
+	`program_id` VARCHAR(255) NOT NULL PRIMARY KEY,
+	`input_lock_timeout` BIGINT NOT NULL
+);
+
+CREATE TABLE `account`(
+	`organization_id` VARCHAR(255) NOT NULL PRIMARY KEY,
+	`name` VARCHAR(255) NOT NULL,
+	`description` VARCHAR(255) NOT NULL,
+	`account_was_verified` BOOL NOT NULL,
+	`username` VARCHAR(255) NOT NULL UNIQUE,
+	`password_hash` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `refresh_token`(
+	`token_id` VARCHAR(255) NOT NULL PRIMARY KEY,
+	`user_id` VARCHAR(255) NOT NULL
+);
+
