b93e22663bbfe48dd697e253f7758f3bb5652a0a
Agustin <aguscambieno@gmail.com>
2024-04-30 00:34:20 -0300

    Fix typo in executor client folder name

--- server/src/components/program/route.rs
+++ server/src/components/program/route.rs
@@ -6,11 +6,14 @@ use super::controller::ProgramController;
 pub fn program_router(path_prefix: &str) -> Scope {
     // web::scope(path_prefix)
     web::scope(path_prefix).
+        // get
         route("all", web::get().to(ProgramController::get_unfiltered_programs)).
         route("{program_id}", web::get().to(ProgramController::download_program)).
         route("inputs/{program_id}", web::get().to(ProgramController::retrieve_input_group)).
         route("program-and-inputs/{program_id}", web::get().to(ProgramController::retrieve_program_and_input_group)).
         route("organization/{organization_id}", web::get().to(ProgramController::get_organization_programs)).
+
+        // post
         route("upload", web::post().to(ProgramController::upload_program).wrap(ValidateJwtMiddleware)).
         route("inputs/{program_id}", web::post().to(ProgramController::add_inputs_group).wrap(ValidateJwtMiddleware))
 }
