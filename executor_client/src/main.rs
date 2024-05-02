512bb8a87e2d55d8bcf372b1d0c9cb0948593d55
Agustin <aguscambieno@gmail.com>
2024-05-02 01:21:09 -0300

    Set up simple clap example

--- executor_client/src/main.rs
+++ executor_client/src/main.rs
@@ -2,6 +2,8 @@ use std::env;
 use std::fs::File;
 use std::io;
 use tar::{Builder, Archive};
+// use clap::{crate_name};
+use clap::{crate_name, Parser, Subcommand};
 
 fn compress_folder(folder_path: &str, output_path: &str) -> io::Result<()> {
     let file = File::create(output_path)?;
@@ -35,28 +37,86 @@ fn decompress_tar(tar_path: &str, output_folder: &str) -> io::Result<()> {
     Ok(())
 }
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    Get {
+        /// a command
+        #[clap(short = 'a', long = "keysa")]
+        valuea: Option<String>,
+
+        #[clap(short = 'b', long = "keysb")]
+        valueb: Option<String>,
+    },
+    Set {
+        key: String,
+        value: String,
+    }
+}
+
 fn main() {
-    // Get command line arguments
-    let args: Vec<String> = env::args().collect();
+    // // Get command line arguments
+    // let args: Vec<String> = env::args().collect();
 
-    println!("{}", args.len());
+    // println!("{}", args.len());
 
-    // Check if the correct number of arguments is provided
-    if args.len() != 3 {
-        eprintln!("Usage: cargo run <folder_path> <output_path>");
-        std::process::exit(1);
-    }
-    let folder_path = &args[1];
-    let output_path = &args[2];
+    // // Check if the correct number of arguments is provided
+    // if args.len() != 3 {
+    //     eprintln!("Usage: cargo run <folder_path> <output_path>");
+    //     std::process::exit(1);
+    // }
+    // let folder_path = &args[1];
+    // let output_path = &args[2];
 
-    // Call the function to compress the folder
-    match compress_folder(folder_path, output_path) {
-        Ok(_) => println!("Folder compressed successfully."),
-        Err(err) => eprintln!("Error: {}", err),
+    // // Call the function to compress the folder
+    // match compress_folder(folder_path, output_path) {
+    //     Ok(_) => println!("Folder compressed successfully."),
+    //     Err(err) => eprintln!("Error: {}", err),
+    // }
+    // match decompress_tar(output_path, &format!("./test/{}.", output_path)) {
+    // // match decompress_tar(output_path, output_path) {
+    //     Ok(_) => println!("Folder decompressed successfully."),
+    //     Err(err) => eprintln!("Error: {}", err),
+    // }
+
+    loop {
+        let mut buf = format!("{} ", crate_name!());
+        
+        std::io::stdin().read_line(&mut buf).expect("Couldn't parse stdin");
+        let line = buf.trim();
+
+        println!("Line value: {}", line);
+
+        let args = shlex::split(line).ok_or("error: Invalid quoting").unwrap();
+
+        println!("{:?}" , args);
+
+        match Args::try_parse_from(args.iter()).map_err(|e| e.to_string()) {
+            Ok(cli) => {
+                match cli.cmd {
+                    Commands::Get{valuea, valueb} => {
+                        if (valuea.is_some()) {
+                            println!("Get valuea: {}", valuea.unwrap());
+                        }
+                        if (valueb.is_some()) {
+                            println!("Get valueb: {}", valueb.unwrap());
+                        }
+                    },
+                    Commands::Set{key, value} => {
+                        println!("Set key: {}, value: {}", key, value);
+                    },
+               }
+            }
+            Err(_) => {
+                println!("That's not a valid command!");
+            }
+       };
     }
-    match decompress_tar(output_path, &format!("./test/{}.", output_path)) {
-    // match decompress_tar(output_path, output_path) {
-        Ok(_) => println!("Folder decompressed successfully."),
-        Err(err) => eprintln!("Error: {}", err),
-    }
+
 }
