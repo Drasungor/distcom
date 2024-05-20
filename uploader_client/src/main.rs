8b47887897197bd776ecd6b1734389d7bc9ed008
Agustin <aguscambieno@gmail.com>
2024-05-19 23:58:22 -0300

    General uploader client setup

--- uploader_client/src/main.rs
+++ uploader_client/src/main.rs
@@ -3,6 +3,9 @@ use std::fs::File;
 use std::io;
 use tar::{Builder, Archive};
 
+mod services;
+mod common;
+
 fn compress_folder(folder_path: &str, output_path: &str) -> io::Result<()> {
     let file = File::create(output_path)?;
     let mut builder = Builder::new(file);
@@ -36,27 +39,7 @@ fn decompress_tar(tar_path: &str, output_folder: &str) -> io::Result<()> {
 }
 
 fn main() {
-    // Get command line arguments
-    let args: Vec<String> = env::args().collect();
 
-    println!("{}", args.len());
 
-    // Check if the correct number of arguments is provided
-    if args.len() != 3 {
-        eprintln!("Usage: cargo run <folder_path> <output_path>");
-        std::process::exit(1);
-    }
-    let folder_path = &args[1];
-    let output_path = &args[2];
-
-    // Call the function to compress the folder
-    match compress_folder(folder_path, output_path) {
-        Ok(_) => println!("Folder compressed successfully."),
-        Err(err) => eprintln!("Error: {}", err),
-    }
-    match decompress_tar(output_path, &format!("./test/{}.", output_path)) {
-    // match decompress_tar(output_path, output_path) {
-        Ok(_) => println!("Folder decompressed successfully."),
-        Err(err) => eprintln!("Error: {}", err),
-    }
+    
 }
