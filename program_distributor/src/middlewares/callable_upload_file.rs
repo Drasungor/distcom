4a9b61208f60b5265d039c1c55fad5d4de989fa5
Agustin <aguscambieno@gmail.com>
2024-06-08 21:16:19 -0300

    Do some file upload refactoring

--- program_distributor/src/middlewares/callable_upload_file.rs
+++ program_distributor/src/middlewares/callable_upload_file.rs
@@ -97,34 +97,38 @@ T: DeserializeOwned,
 
 
         if (field_is_file) {
-            let file_suffix = get_file_suffix(&filename);
-            let new_filename = format!("{}.{}", Uuid::new_v4(), file_suffix);
+            // let file_suffix = get_file_suffix(&filename);
+            // let new_filename = format!("{}.{}", Uuid::new_v4(), file_suffix);
 
-            // Define the file path where you want to save the uploaded file
-            let file_path = format!("{}/{}", uploads_folder, new_filename);
-            let file_path_clone = file_path.clone();
-            // Create a new file and write the field data to it
-            let f = web::block(|| File::create(file_path_clone)).await.
-                                expect("Error in file creation task").expect("Error in file creation");
-            while let Some(chunk) = field.try_next().await.expect("Error in file chunk get") {
-                let mut file_pointer_clone = f.try_clone().expect("Error in file pointer clone");
-                web::block(move || file_pointer_clone.write_all(&chunk)).await.
-                        expect("Error chunk creation task").expect("Error in chunk creation");
-            }
+            // // Define the file path where you want to save the uploaded file
+            // let file_path = format!("{}/{}", uploads_folder, new_filename);
+            // let file_path_clone = file_path.clone();
+            // // Create a new file and write the field data to it
+            // let f = web::block(|| File::create(file_path_clone)).await.
+            //                     expect("Error in file creation task").expect("Error in file creation");
+            // while let Some(chunk) = field.try_next().await.expect("Error in file chunk get") {
+            //     let mut file_pointer_clone = f.try_clone().expect("Error in file pointer clone");
+            //     web::block(move || file_pointer_clone.write_all(&chunk)).await.
+            //             expect("Error chunk creation task").expect("Error in chunk creation");
+            // }
+            
+            let new_filename = process_file_field(field, uploads_folder, &filename).await;
             files_names.push(new_filename);
         } else {
             // TODO: remove assert and manage the error properly
             assert!(received_object.is_none(), "More than one data attribute was received");
-
             let mut json_str_bytes = bytes::BytesMut::from("");
 
-            while let Some(chunk) = field.try_next().await.expect("Error in file chunk get") {
-                json_str_bytes.extend_from_slice(&chunk);
-            }
-            let bytes_array = json_str_bytes.to_vec();
-            let u8_vector = bytes_array.as_slice();
-            let json_str = std::str::from_utf8(u8_vector).expect("Error in conversion to json string");
-            received_object = Some(serde_json::from_str::<T>(&json_str).expect("Error in conversion from string to struct"));
+
+            // while let Some(chunk) = field.try_next().await.expect("Error in file chunk get") {
+            //     json_str_bytes.extend_from_slice(&chunk);
+            // }
+            // let bytes_array = json_str_bytes.to_vec();
+            // let u8_vector = bytes_array.as_slice();
+            // let json_str = std::str::from_utf8(u8_vector).expect("Error in conversion to json string");
+            // received_object = Some(serde_json::from_str::<T>(&json_str).expect("Error in conversion from string to struct"));
+            
+            received_object = Some(process_text_object_field::<T>(field).await);
         }
     }
     return Ok((files_names, received_object.expect("No body was received")));
@@ -146,4 +150,19 @@ async fn process_file_field(mut field: actix_multipart::Field, uploads_folder: &
                 expect("Error chunk creation task").expect("Error in chunk creation");
     }
     return new_filename;
+}
+
+async fn process_text_object_field<T>(mut field: actix_multipart::Field) -> T 
+where
+T: DeserializeOwned,
+{
+    let mut json_str_bytes = bytes::BytesMut::from("");
+
+    while let Some(chunk) = field.try_next().await.expect("Error in file chunk get") {
+        json_str_bytes.extend_from_slice(&chunk);
+    }
+    let bytes_array = json_str_bytes.to_vec();
+    let u8_vector = bytes_array.as_slice();
+    let json_str = std::str::from_utf8(u8_vector).expect("Error in conversion to json string");
+    return serde_json::from_str::<T>(&json_str).expect("Error in conversion from string to struct");
 }
