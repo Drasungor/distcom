a5e325293add39fdd03bdf5134ca1202183692a1
Agustin Cambiano <aguscambiano@gmail.com>
2024-07-28 22:16:08 -0300

    Delete unused functions

--- executor_client/src/main.rs
+++ executor_client/src/main.rs
@@ -5,7 +5,6 @@ use utils::process_inputs::process_page_size;
 
 use crate::commands::get_organizations::select_organizations;
 use crate::commands::get_programs::select_general_programs;
-use crate::utils::compression::decompress_tar;
 use crate::utils::process_inputs::process_user_input;
 
 mod common;
@@ -13,52 +12,6 @@ mod commands;
 mod models;
 mod utils;
 mod services;
-
-
-
-async fn run_program_get_example(program_id: String) {
-
-
-    let request_url = format!("http://localhost:8080/program/{}", program_id);
-
-    let response = reqwest::get(request_url).await.expect("Error in get");
-
-    // Ensure the request was successful (status code 200)
-    if response.status().is_success() {
-        // Open a file to write the downloaded content
-        let mut file = File::create("downloaded_file.tar").expect("Error in file creation");
-        file.write_all(response.bytes().await.expect("Error in bytes get").as_ref()).expect("Errors in file write");
-
-        decompress_tar("./downloaded_file.tar", "./src/runner/methods");
-        
-        println!("File downloaded successfully!");
-    } else {
-        println!("Failed to download file: {}", response.status());
-    }
-}
-
-
-
-async fn get_program_template() {
-    let request_url = "http://localhost:8080/program/template";
-    let response = reqwest::get(request_url).await.expect("Error in get");
-
-    // Ensure the request was successful (status code 200)
-    if response.status().is_success() {
-        // Open a file to write the downloaded content
-        let mut file = File::create("downloaded_file.tar").expect("Error in file creation");
-        file.write_all(response.bytes().await.expect("Error in bytes get").as_ref()).expect("Errors in file write");
-
-        decompress_tar("./downloaded_file.tar", "./src/runner/methods");
-        
-        println!("File downloaded successfully!");
-    } else {
-        println!("Failed to download file: {}", response.status());
-    }
-}
-
-
-
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
