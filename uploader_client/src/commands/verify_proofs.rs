afc0e46f765ecc3501e824f914843639f2c5ca15
Agustin Cambiano <aguscambiano@gmail.com>
2024-07-28 23:23:24 -0300

    .

--- uploader_client/src/commands/verify_proofs.rs
+++ uploader_client/src/commands/verify_proofs.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 use clap::{Parser, Subcommand};
 use std::{fs, process::Command};
 
-use crate::{common, models::{returned_input_group::print_input_groups_list, returned_program::print_programs_list}, services::program_distributor::{PagedProgramInputGroups, PagedPrograms}, utils::process_inputs::{process_previously_set_page_size, process_user_input}};
+use crate::{common, models::{returned_input_group::print_input_groups_list, returned_program::print_programs_list}, services::program_distributor::{PagedProgramInputGroups, PagedPrograms}, utils::{process_inputs::{process_previously_set_page_size, process_user_input}, verifying::{retrieve_proven_inputs, verify_all_program_proven_executions, verify_proven_execution, verify_some_program_proven_executions}}};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -32,79 +32,6 @@ enum GetProofsCommands {
     Back,
 }
 
-// async fn retrieve_proven_inputs(program_id: &str, limit: Option<usize>, page: Option<usize>) -> PagedProgramInputGroups {
-async fn retrieve_proven_inputs(program_id: &str, limit: usize, page: usize) -> PagedProgramInputGroups {
-    let mut write_guard = common::config::PROGRAM_DISTRIBUTOR_SERVICE.write().expect("Error in rw lock");
-    return write_guard.get_program_proven_inputs(program_id, Some(limit), Some(page)).await.expect("Error while getting uploaded programs");
-}
-
-
-async fn verify_proven_execution(program_id: &str, input_group_id: &str) {
-    let mut write_guard = common::config::PROGRAM_DISTRIBUTOR_SERVICE.write().expect("Error in rw lock");
-    write_guard.download_program(program_id, Path::new("./src/runner/methods")).await;
-
-    // let download_path = "../downloads/proof.bin";
-    let download_path = "./downloads/proof.bin";
-    write_guard.download_proof(program_id, input_group_id, Path::new(download_path)).await.expect("Error while downloading proof");
-    // pub async fn download_proof(&mut self, program_id: &str, input_group_id: &str, download_path: &Path) -> Result<(), EndpointError> {
-
-
-    println!("Starting proof verification");
-
-    let execution_args = vec![program_id, input_group_id];
-
-    let output = Command::new("cargo")
-        .arg("run")
-        .args(execution_args)
-        .current_dir("./src/runner")
-        .output()
-        .expect("Failed to execute child program");
-
-    // println!("Program output: {:?}", output);
-
-    if output.status.success() {
-        println!("Proof verified successfully.");
-        // println!("Output: {}", String::from_utf8(output.stdout).unwrap());
-        // println!("Stderr: {}", String::from_utf8(output.stderr).unwrap());
-        write_guard.confirm_proof_validity(program_id, input_group_id).await.expect("Error confirming proof validity");
-    } else {
-        println!("Process failed.");
-        write_guard.mark_proof_as_invalid(program_id, input_group_id).await.expect("Error while marking proof as invalid");
-    }
-
-}
-
-
-async fn verify_all_program_proven_executions(program_id: &str) {
-    let max_page_size = common::config::CONFIG_OBJECT.max_page_size;
-    let mut input_groups_page = retrieve_proven_inputs(program_id, max_page_size, 1).await;
-    let mut input_groups_array = input_groups_page.program_input_groups;
-    while input_groups_array.len() != 0 {
-        for input_group_proof in input_groups_array {
-            verify_proven_execution(program_id, &input_group_proof.input_group_id).await;
-        }
-        input_groups_page = retrieve_proven_inputs(program_id, max_page_size, 1).await;
-        input_groups_array = input_groups_page.program_input_groups;
-    }
-}
-
-async fn verify_some_program_proven_executions(program_id: &str, proofs_amount: usize) {
-    let max_page_size = common::config::CONFIG_OBJECT.max_page_size;
-    let mut input_groups_page = retrieve_proven_inputs(program_id, max_page_size, 1).await;
-    let mut input_groups_array = input_groups_page.program_input_groups;
-    let mut verified_proofs = 0;
-    while input_groups_array.len() != 0 && verified_proofs < proofs_amount {
-        let mut current_page_iterator = 0;
-        while current_page_iterator < input_groups_array.len() && verified_proofs < proofs_amount {
-            let input_group_proof = &input_groups_array[current_page_iterator];
-            verify_proven_execution(program_id, &input_group_proof.input_group_id).await;
-            current_page_iterator += 1;
-            verified_proofs += 1;
-        }
-        input_groups_page = retrieve_proven_inputs(program_id, max_page_size, 1).await;
-        input_groups_array = input_groups_page.program_input_groups;
-    }
-}
 
 pub async fn select_proven_inputs(program_id: &str, first_received_limit: usize, first_received_page: usize) {
     let mut should_continue_looping = true;
--- uploader_client/src/utils/mod.rs
+++ uploader_client/src/utils/mod.rs
@@ -1,3 +1,4 @@
 pub mod compression;
+pub mod verifying;
 pub mod process_inputs;
 pub mod local_storage_helpers;
