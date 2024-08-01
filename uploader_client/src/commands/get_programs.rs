9770005fbbea30f40d7850ec84e65122c67be20a
Agustin Cambiano <aguscambiano@gmail.com>
2024-08-01 18:38:07 -0300

    Add upload multiple input groups command

--- uploader_client/src/commands/get_programs.rs
+++ uploader_client/src/commands/get_programs.rs
@@ -40,6 +40,27 @@ async fn post_input_group(program_id: &str, uploaded_input_group_file_path: &Pat
     return write_guard.upload_input_group(program_id, uploaded_input_group_file_path).await.expect("Error while uploading program input group");
 }
 
+async fn manage_input_group_upload(program_id: &str, uploaded_input_group_file_path: &Path) {
+    let input_group_id = post_input_group(program_id, uploaded_input_group_file_path).await;
+    let input_group_folder = format!("./programs_data/{program_id}/{input_group_id}");
+    create_folder(&input_group_folder);
+    let final_input_group_path = format!("{input_group_folder}/{}", uploaded_input_group_file_path.file_name().unwrap().to_str().unwrap());
+    fs::copy(uploaded_input_group_file_path, final_input_group_path).expect("Error moving input file");
+    println!("Uploaded input group with path: {}", );
+}
+
+async fn upload_inputs_folder(program_id: &str, folder_path: &Path) {
+    let dir_entries = fs::read_dir(folder_path).expect("Failed reading the downloads folder");
+    for entry in dir_entries {
+        let dir_entry = entry.expect("Error in entry parsing");
+        let current_path = dir_entry.path();
+        let extension_option = current_path.extension();
+        if current_path.is_file() && extension_option.is_some() && extension_option.unwrap() == "csv" {
+            manage_input_group_upload(program_id, &current_path).await;
+        }
+    }
+}
+
 // TODO: Update this so that the page size is used
 pub async fn select_my_programs(first_received_limit: usize, first_received_page: usize) -> bool {
     // let mut should_continue_looping = true;
@@ -63,14 +84,14 @@ pub async fn select_my_programs(first_received_limit: usize, first_received_page
                     GetProgramsCommands::PostInput{index, input_file_name} => {
                         let chosen_program = &programs_page.programs[index];
                         let program_id = &chosen_program.program_id;
-                        // let input_file_path = Path::new(&input_file_path);
                         let input_file_path_string = format!("./uploads/{input_file_name}");
                         let input_file_path = Path::new(&input_file_path_string);
-                        let input_group_id = post_input_group(program_id, input_file_path).await;
-                        let input_group_folder = format!("./programs_data/{program_id}/{input_group_id}");
-                        create_folder(&input_group_folder);
-                        let final_input_group_path = format!("{input_group_folder}/{}", input_file_path.file_name().unwrap().to_str().unwrap());
-                        fs::copy(input_file_path, final_input_group_path).expect("Error moving input file");
+                        
+                        if input_file_path.is_dir() {
+                            upload_inputs_folder(program_id, input_file_path).await;
+                        } else {
+                            manage_input_group_upload(program_id, input_file_path).await;
+                        }
                     },
                     GetProgramsCommands::Back => {
                         // should_continue_looping = false;
@@ -85,7 +106,7 @@ pub async fn select_my_programs(first_received_limit: usize, first_received_page
             Err(_) => {
                 println!("That's not a valid command!");
             }
-       };
+        };
         print_programs_list(&programs_page.programs);
 
     }    
